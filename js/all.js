9487a96d99240e4e7f9bbe5761c4bb9d12a323fe
neohyntw <neohyntw@gmail.com>
2024-05-27 23:28:28 +0800

    commit
    add homepage

--- js/all.js
+++ js/all.js
@@ -7,4 +7,41 @@ $(document).ready(function(){
       $('.modal').toggleClass('close');
 
     });
+
+    $('.card .stretched-link').click(function(e){
+      e.preventDefault()
+      console.log(e.target)
+      $('.modal').toggleClass('close');
+      $('.modal-showcase').toggleClass('close');
+    })
+
+    $('.modal-showcase a').click(function(e){
+      e.preventDefault()
+      console.log(e.target)
+      $('.modal').toggleClass('close');
+      $('.modal-showcase').toggleClass('close');
+    })
+
+    $('.article-list').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        responsive: [
+          {
+            breakpoint: 987,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          }]
+    });
+
+
+    
+
   });
+
